1e8c212d21caa78364a6dac37c7f5023ebc5fa28
Administrator <js1081996@gmail.com>
2025-03-22 16:49:29 +0530

    Add project files.

---
+++ EmployeeDetails/Controllers/EmployeesController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection.Emit;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace EmployeeDetails.Controllers
+{
+    
+    public class EmployeesController : ApiController
+    {
+
+        public HttpResponseMessage Get(string gender)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                switch(gender.ToLower())
+                {
+                    case "all":
+                        return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.ToList());
+                    case "male":
+                        return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.Where(e=> e.Gender.ToLower()== "male").ToList());
+                    case "female":
+                        return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.Where(e => e.Gender.ToLower()== "female") .ToList());
+                    default:
+                        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "NO such employee exist for the gender");
+                }
+
+            }
+        }
+        
+        public HttpResponseMessage LoadAllEmployeeByID(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with ID  : =  " + id.ToString() + " Not found");
+                }
+
+            }
+
+        }
+        public HttpResponseMessage post([FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+
+            }
+
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (entity == null)
+
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with Id : =" + id.ToString() + "NOt found ");
+
+
+
+                    }
+                    else
+                    {
+                        entities.Employees.Remove(entity);
+                        return Request.CreateResponse(HttpStatusCode.OK, entities.SaveChanges());
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+
+            }
+
+
+        }
+        public HttpResponseMessage Put(int id, [FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with ID : = " + id.ToString() + "Not found");
+                    }
+                    else
+                    {
+                        entity.FirstName = employee.FirstName;
+                        entity.LastName = employee.LastName;
+                        entity.FirstName = employee.FirstName;
+                        entity.LastName = employee.LastName;
+
+
+
+                        return Request.CreateResponse(HttpStatusCode.OK, entities.SaveChanges());
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
